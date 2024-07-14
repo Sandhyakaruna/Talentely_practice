424bc7a3c91352595cdc7e0723128b03575c4368
Sandhya K <115928773+Sandhyakaruna@users.noreply.github.com>
2024-07-14 21:00:20 +0530

    Create PasswordStrength.java
    isStrongPassword Function:
    
    1.Takes a char * password as input.
    2.Initializes four variables to check for the presence of upper case letters, lower case letters, digits, and special characters.
    3.Checks if the length of the password is at least 10. If not, it returns 0 indicating a weak password.
    4.Iterates through the password characters to set the corresponding flags if the character types are found.
    5.Returns 1 if all required character types are present; otherwise, returns 0.
    
    
    
    main Function:
    
    1.Takes a password input from the user.
    2.Calls the isStrongPassword function to determine if the password is strong or weak.
    3.Prints the appropriate message based on the result.

---
+++ PasswordStrength.java
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int isStrongPassword(char *password) {
+    int hasUpper = 0, hasLower = 0, hasDigit = 0, hasSpecial = 0;
+    int length = strlen(password);
+
+    if (length < 10) {
+        return 0; // Weak password due to insufficient length
+    }
+
+    for (int i = 0; i < length; i++) {
+        if (isupper(password[i])) {
+            hasUpper = 1;
+        } else if (islower(password[i])) {
+            hasLower = 1;
+        } else if (isdigit(password[i])) {
+            hasDigit = 1;
+        } else {
+            hasSpecial = 1;
+        }
+    }
+
+    return hasUpper && hasLower && hasDigit && hasSpecial;
+}
+
+int main() {
+    char password[100001];
+    printf("Enter password: ");
+    scanf("%100000s", password);
+
+    if (isStrongPassword(password)) {
+        printf("Strong password\n");
+    } else {
+        printf("Weak password\n");
+    }
+
+    return 0;
+}
