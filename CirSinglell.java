de55673d4b05147d18fcbc3d071ff904b70bd0eb
Sandhya K <115928773+Sandhyakaruna@users.noreply.github.com>
2024-07-19 15:57:20 +0530

    Create CirSinglell.java

---
+++ CirSinglell.java
@@ -0,0 +1,51 @@
+class cll{
+        Node last;
+
+        class Node{
+            int data;
+            Node next;
+
+            Node(int val){
+                data = val;
+                next = null;
+            }
+        }
+        cll(){
+            last = null;
+        }
+
+        public void insertbegin(int k) {
+            Node newnode=new Node(k);
+
+            if( last==null) {
+                newnode.next = newnode;
+                last = newnode;
+                
+            }else {
+                newnode.next = last.next;
+                last.next = newnode;
+            }
+        }
+        public void display(){
+            Node temp = last.next;
+            do{
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            while(temp!= last.next);
+        }
+        public static void main(String[] args) {
+            Scanner s=new Scanner(System.in);
+            cll list=new cll();
+            while(true){
+                int j = s.nextInt();
+                if(j==-1){
+                    break;
+                }else{
+                    list.insertbegin(j);
+                }
+            }
+            
+            list.display();
+        }
+    }
