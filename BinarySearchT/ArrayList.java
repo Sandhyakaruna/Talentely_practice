8ca96591b81864a06a7c500ca7a25f2c9588f7eb
Sandhya K <sandhyamanju263@gmail.com>
2024-08-15 22:54:37 +0530

    Created ArrayList

---
+++ BinarySearchT/ArrayList.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.ArrayList;
+class String
+{
+    publicstatic void main(String[] args)
+    {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(5);
+        list.add(34);
+        list.add(5);
+        ArrayList<String> Str= new ArrayList<>();
+        Str.add("Sandy");
+        Str.add("Alone");
+        ArrayList<Object> obj=new ArrayList<>();
+        obj.add(7);
+        obj.add("All");
+        System.out.println(list);
+        System.out.println(Str);
+        System.out.println(obj);
+
+    }
+}
