e152c8feb19750fdd35022fef255a71101764a3e
Sandhya K <115928773+Sandhyakaruna@users.noreply.github.com>
2024-09-12 21:14:31 +0530

    Create SequenceNumber.py

---
+++ SequenceNumber.py
@@ -0,0 +1,15 @@
+class NumberGenerator:
+    def __init__(self, start=0, end=10):
+        self.start = start
+        self.end = end
+
+    def generate_numbers(self):
+        current = self.start
+        while current <= self.end:
+            yield current
+            current += 1
+
+# Usage
+gen = NumberGenerator(1, 5)
+for number in gen.generate_numbers():
+    print(number)
